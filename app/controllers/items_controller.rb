236934940da9591cb86651b36cfc102fcaece2fd
Amy May Nguyen <anguye10@uchicago.edu>
2025-02-17 02:02:56 +0000

    Completed challenge

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,17 @@ class ItemsController < ApplicationController
 
     render({ :template => "item_templates/list" })
   end
+
+  def show_form
+    render({ :template => "item_templates/new_item_form" })
+  end
+  
+  def add_item
+    new_item = Item.new
+    new_item.link_url = params["link_url"]
+    new_item.link_description = params["link_description"]
+    new_item.thumbnail_url = params["thumbnail_url"]
+    new_item.save    
+    redirect_to("/")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
 
   get("/", { :controller => "items", :action => "index" })
+  get("/backdoor", { :controller => "items", :action => "show_form" })
+  post("/backdoor/new_item", { :controller => "items", :action => "add_item" })
+
 end
